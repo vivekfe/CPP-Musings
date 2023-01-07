de2fb499ceb044c066833e03af8ab15b51a9640a
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-07 10:02:41 +0100

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+struct stack 
+{
+
+int top;
+int a[10];
+
+};
+
+void push()
+{
+// pass
+}
+
+void pop()
+{
+//pass
+}
+
+int main()
+{
+  struct stack s1;
+  s1.top = -66  // No importance given to data and more stress on procedures
+  return 0;
+}
+
+// What if we use the object oriented solution
+
+// structures in c++ have been made more capable
+
+struct stack 
+{
+private:
+  
+int top;
+int a[10];
+public:
+  void pop()
+  {
+    // pass  
+  }
+  
+};
+
+// in c++ we are allowed to drop this struct keyword however in C you have been forced to say struct stack s1, s2, in c++ it is optional
+
+stack s1, s2;
+
+s1.top = -1 // This is not allowed in this particular construct
