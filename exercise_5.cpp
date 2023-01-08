4d51707ed34a4bac43f424088e3e507316246e3c
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-08 16:59:25 +0100

    Update exercise_5.cpp

--- exercise_5.cpp
+++ exercise_5.cpp
@@ -1,5 +1,5 @@
-// shared responsibility is no one's responsibility
-// so compiler says Either you do all of them or I do it
+// joint responsibility is no one's responsibility (like I create a 2 arg constructor but don't leave 0 arg constructor to be created by compiler)
+// so compiler says Either you do it or I do it
 // if you do, then it is your responsibility to declare even the 0-argument constructor
 
 // ctrl+F5 to compile all files in one project to compile in one shot
@@ -30,23 +30,48 @@ p=(int *) &s;
 // ugly implementation of stack in C
 struct stack
 {
-int top;
-int a[10];
+  int top;
+  int a[10];
 };
 void init(struct stack *s)
 {
-s->top = 0;
-  
+  s->top = 0;
 }
 void push(struct stack *s, int x)
 {
-s->a[s->top]=x;
-s->top++;
+  s->a[s->top]=x;
+  s->top++;
 }
 
 void main()
 {
-stack s1, s2;
-init(&s1);init(&s2);
-push(&s1,10);
+  
+}
+
+
+/* Constructors */
+
+class ex{
+private: int i; float j;
+
+  public:
+  ex(int ii, float jj)
+    {
+      i=ii;
+      i=jj;
+    }
+  ex()
+  {
+    i=0;
+    j=0.0;
+  }
+};
+void main()
+  
+{
+  ex e1(10,3.14);
+  ex e2;
+  // object creation is a two step process, first the object is created by allocating the space in memory (compiler emits the instructions ) 
+  // and then constructor is called 
+  // o args constructor will always be there in the class and compiler will create 
 }
