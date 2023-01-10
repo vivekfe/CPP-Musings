109c4b25270e7748f45aa8c40aacb1dbca2098b1
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-10 08:01:34 +0100

    adding program organization

--- exercise_10.cpp
+++ exercise_10.cpp
@@ -1,1 +1,46 @@
+// program organization
 
+// cin was an object of istream_withassign (class)
+
+// cout was an object of ostream_withassign (class)
+
+// These classes come from iostream.h as it contains the decalaration of these two classes
+
+// these two objects also come from iostream.h
+
+
+// comp.h - here we simply want to declare the methods and class variables, this is
+// simply prototype declaration
+
+class comp 
+{
+  private: 
+    double r, i;
+  public:
+    comp(double rr=0, double ii=0.0); // default values in declaration, we need to indicate it here
+                                      // do not try to give defualt values in definition
+    void print();
+    comp operator + comp();
+}
+
+// we just share .h file but don't share the secret sauce of our class
+
+#include "comp.h"
+#include <iostream.h>
+comp:: comp (double rr, double ii)
+{
+  r=rr;
+  i=ii;
+}
+void comp:: print()
+{
+  cout << r << i;
+}
+
+comp comp:: operator+(comp &c2)
+{
+  comp t;
+  t.r=r+c2.r;
+  t.i=i+c2.i;
+  return t;
+}
