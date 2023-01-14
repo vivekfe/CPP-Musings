332196d695b993d302cd0abe519d462c721447b1
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-14 17:07:12 +0100

    Example showing usage of const functions

---
+++ BoQian/ConstAndFunctions.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class ConstExample
+{
+    private:
+        int age;
+        string name;
+    public:
+        ConstExample()
+            {
+                age= 3;
+                name= "Dummy";
+            }
+        const string & getName() {return name;}
+        void printName() const 
+        {
+            std:: cout << "const Version" << std::endl;
+            
+        }
+        void printName()
+        {
+            std:: cout << "non const Version" << std::endl;
+            
+        }
+         void ConstCanCallOnlyConst() const
+        {
+            printName();
+            
+        }
+    
+};
+int main()
+{
+    ConstExample d;
+    d.printName();
+    const ConstExample g;
+    g.printName();
+    g.ConstCanCallOnlyConst();
+    return 0;
+    
+}
