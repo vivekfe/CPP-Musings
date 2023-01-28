a8292f31e906c48f4e318ac8eddb10712476da58
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-28 12:59:42 +0100

    Update InConsistencyOfConstructors.cpp

--- AdhocCPP/InConsistencyOfConstructors.cpp
+++ AdhocCPP/InConsistencyOfConstructors.cpp
@@ -1,34 +1,45 @@
+
 #include <iostream>
 
 class X
 {
-    public:
-     int x;
-    X()=default;
-  X( int value ) : x(value) {
-      std:: cout << "Default constructor called !" << std::endl;
-  }
- 
+public:
+    int x;
+    X() {
+        std::cout << "Default constructor called !" << std::endl;
+
+    }
+
+    X(int value) : x(value) {
+        std::cout << "Value constructor called !" << std::endl;
+    }
+
 };
 
 struct Y
 {
-  Y( const X& x ) : y(x.x) {}
-  int y;
+    Y(const X& x) : y(x.x) {}
+    int y;
 };
 
 int main()
 {
 
-X m(5);
-X y();  // this will fail and ask to emove parentesis for default initialization
-X w;  // this will fail and ask to emove parentesis for default initialization
-X z;   // this will work fine as it will create a default initialization
-X *p1= new X();  // these will be okay as it is not considered a function call 
-X* p2= new X();
-std::cout << m.x;
-std::cout << w.x;  // Error	C4700	uninitialized local variable 'y' 
+    X m{ 5 };
+    std::cout << "Ms Value is " << m.x << std::endl;
+    X t;  // this be okay as it appears as a function declaration
+    X w;   // this will work fine as it will create a default initialization
+    std::cout << "Ws Value is " << w.x << std::endl;
+    X* p1 = new X();  // these will be okay as it is not considered a function call and call default
+    X* p2 = new X;   // this will also call default constructor
 
-int z();  // Unlike y() this works fine
-return 0;
+    std::cout << "Ts Value is " << t.x << std::endl;  // will throw an error
+    int z{};  // Unlike y() this works fine
+    std::cout << "Zs Value is " << z << std::endl;  // will throw an error
+    int A{};  // This is uniform initialization
+    std::cout << "Zs Value is " << A << std::endl;  // will throw an error
+
+   //  int x{1.888}; //will NOT compile and save trouble.
+
+    return 0;
 }
