0adb90d75775cc107a8353f10439bd97a6971117
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-28 11:50:04 +0100

    Create PureVirtualFunction.cpp

---
+++ AdhocCPP/PureVirtualFunction.cpp
@@ -0,0 +1,14 @@
+class Shape {
+public:
+    virtual double area() = 0;
+};
+
+class Rectangle : public Shape {
+private:
+    double length, width;
+public:
+    Rectangle(double l, double w) : length(l), width(w) {}
+    double area() { return length * width; }
+};
+
+//In C++, a pure virtual function is a virtual function that has no implementation in the base class, and must be overridden in derived classes. A pure virtual function is declared by using the "= 0" notation at the end of the function declaration.
